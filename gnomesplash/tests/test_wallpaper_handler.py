3fd2c9fc69220fb0c9b440b93072d147da2cf3bb
richiestuver <stuverr@oregonstate.edu>
2021-07-04 18:25:37 -0700

    working on download image method, need to implement create intermediate level directories on save.

--- gnomesplash/tests/test_wallpaper_handler.py
+++ gnomesplash/tests/test_wallpaper_handler.py
@@ -8,13 +8,18 @@ import imghdr  # validate image type
 import logging  # report info to pytest output
 import os  # manage files and dirs on fs
 import os.path  # handle path arguments for saving to fs
+from urllib.parse import urlparse
 from _pytest.nodes import File
 
 import pytest
 from gi.repository import Gio  # see PyObject API
 
+
+# following entities are tested in this module:
 from gnomesplash.wallpaper_handler import update_wallpaper
+from gnomesplash.wallpaper_handler import download_image
 from gnomesplash.wallpaper_handler import WallpaperUpdateError
+from gnomesplash.wallpaper_handler import ImageDownloadError
 
 
 @pytest.fixture
@@ -31,21 +36,29 @@ def wallpaper_settings() -> Gio.Settings:
 def clear_downloads(caplog) -> None:
     # store images downloaded as a result of tests in the project directory only.
     # This directory should be cleared prior to each run to ensure a clean test.
-    downloads_folder = os.path.abspath("gnomesplash/tests/test_data/downloads")
-    if not os.path.exists(downloads_folder):
-        raise FileNotFoundError(
-            f"Directory {downloads_folder} does not exist. Make sure project structure is intact."
-        )
 
-    # scandir returns an iterator of DirEntry objects that contains a representation of each file in the directory
-    # along with useful attributes like name, path, is_file, etc.
-    with os.scandir(downloads_folder) as dir:
-        for file_entry in dir:  # iterator, so continues until StopIteration is raised
-            os.remove(file_entry.path)
+    dirs = [
+        os.path.abspath("gnomesplash/tests/test_data/downloads"),
+        os.path.abspath("gnomesplash/tests/test_data/downloads/extra_dir"),
+    ]
 
-    # show result of test setup in testing output. see caplog info in Pytest docs.
-    caplog.set_level(logging.INFO)
-    logging.info(f"Pre-test setup: {downloads_folder} is empty.")
+    for downloads_folder in dirs:
+        if os.path.exists(downloads_folder):
+            # raise FileNotFoundError(
+            #     f"Directory {downloads_folder} does not exist. Make sure project structure is intact."
+            # )
+
+            # scandir returns an iterator of DirEntry objects that contains a representation of each file in the directory
+            # along with useful attributes like name, path, is_file, etc.
+            with os.scandir(downloads_folder) as dir:
+                for (
+                    file_entry
+                ) in dir:  # iterator, so continues until StopIteration is raised
+                    os.remove(file_entry.path)
+
+        # show result of test setup in testing output. see caplog info in Pytest docs.
+        caplog.set_level(logging.INFO)
+        logging.info(f"Pre-test setup: {downloads_folder} does not exist or is empty.")
 
 
 @pytest.mark.parametrize(
@@ -56,28 +69,94 @@ def clear_downloads(caplog) -> None:
         "https://images.unsplash.com/photo-1558328511-7d6490908755",
     ],
 )
-def test_download_image_success(clear_downloads, img_url):
+def test_download_image_success(clear_downloads, img_url: str):
     """
     Verify that download_image function successfully downloads the target image. Attempt to open the file
     and verify that file downloaded is in fact an image. Filename should be the basename of the img_url
     provided, with an additional extension based on image type. Expect jpg for tests.
     """
-    # TODO: specify file name to save
-    # TODO: call download image function
-    # TODO: attempt to open image
-    # TODO: assert that file is an image. Note: assume jpg for test data.
 
-    assert False
+    file_name = os.path.basename(urlparse(img_url).path)
+    file_path = os.path.join(
+        "gnomesplash/tests/test_data/downloads", f"{file_name}.jpg"
+    )
+
+    download_image(img_url, file_path=file_path)
+
+    with open(file_path, "rb") as file:  # will raise FileNotFound error
+        assert imghdr.what(file) is not None  # None returned for invalid files
 
 
-def test_download_image_failure():
+@pytest.mark.parametrize(
+    "img_url",
+    [
+        "https://images.unsplash.com/photo-1473081556163-2a17de81fc97",
+        "https://images.unsplash.com/photo-1536431311719-398b6704d4cc",
+        "https://images.unsplash.com/photo-1558328511-7d6490908755",
+    ],
+)
+def test_download_image_new_directory(img_url: str):
     """
-    Download image should fail if the target is not a valid url or is not an image. Will also
-    fail if filename already exists at target location. Should raise
+    Verify that download_image function creates a new directory path in the event the target file path does not exist.
+    """
+
+    file_name = os.path.basename(urlparse(img_url).path)
+    file_path = os.path.join(
+        "gnomesplash/tests/test_data/downloads/extra_dir", f"{file_name}.jpg"
+    )
+
+    download_image(img_url, file_path=file_path)
+
+    with open(file_path, "rb") as file:  # will raise FileNotFound error
+        assert imghdr.what(file) is not None  # None returned for invalid files
+
+
+@pytest.mark.parametrize(
+    "img_url",
+    [
+        "https://example.com/images/myphoto.jpg",
+        "https://raw.githubusercontent.com/richiestuver/gnomesplash/master/README.md",
+    ],
+)
+def test_download_image_invalid_failure(img_url: str):
+    """
+    Download image should fail if the target is not a valid url or is not an image. Should raise
     an appropriate error (TBD) instead of failing silently or saving the file to filesystem.
     """
 
-    assert False
+    file_name = os.path.basename(urlparse(img_url).path)
+    file_path = os.path.join(
+        "gnomesplash/tests/test_data/downloads", f"{file_name}.jpg"
+    )
+
+    with pytest.raises(ImageDownloadError):
+        download_image(img_url, file_path)
+
+
+@pytest.mark.parametrize(
+    "img_url",
+    [
+        "https://images.unsplash.com/photo-1473081556163-2a17de81fc97",
+    ],
+)
+def test_download_image_file_exists_failure(img_url: str):
+    """
+    Verify that download_image function does not repeat image download when file at specified
+    path already exists. Function should raise FileNotFound error.
+    """
+
+    file_name = os.path.basename(urlparse(img_url).path)
+    file_path = os.path.join(
+        "gnomesplash/tests/test_data/downloads", f"{file_name}.jpg"
+    )
+
+    # make sure there is a file at the specified path already
+    if not os.path.exists(file_path):
+        with open(file_path, "w"):
+            pass
+
+    with pytest.raises(FileExistsError):
+        download_image(img_url, file_path)
 
 
 @pytest.mark.parametrize(
@@ -94,7 +173,7 @@ def test_download_image_failure():
         ),
     ],
 )
-def test_update_background_success(wallpaper_settings: Gio.Settings, img_path):
+def test_update_background_success(wallpaper_settings: Gio.Settings, img_path: str):
     """
     Load sample images and set the background appropriately. Settings schema for org.gnome.desktop.background
     is stored in wallpaper_settings, a dict-like object provided by PyGObject to interface with GIO library.
--- gnomesplash/wallpaper_handler.py
+++ gnomesplash/wallpaper_handler.py
@@ -13,13 +13,16 @@ More information on this schema can be found at:
 https://github.com/GNOME/gsettings-desktop-schemas/blob/master/schemas/org.gnome.desktop.background.gschema.xml.in 
 """
 
+import os
 import os.path
 import imghdr  # use to determine if image is valid
+import io
+from PIL import Image, UnidentifiedImageError
+
+import requests
 
 # see PyGObject API ref for Gio.Settings or >>> help(Gio.Settings) in REPL
-from gi.repository import (
-    Gio,
-)
+from gi.repository import Gio
 
 
 class WallpaperUpdateError(Exception):
@@ -30,22 +33,57 @@ class WallpaperUpdateError(Exception):
     pass
 
 
-def download_image(url, file_path=""):
+class ImageDownloadError(Exception):
     """
-    Download an image at specified url. This is an API agnostic function that does not
-    take an API key for a particular service. Expectation is that resource is generally
-    accesssible.
-
-    Attempt to save image at target file path. If file path does not exist, it will be created.
-    Default location is user's home directory. Returns the location on filesystem where image was saved.
-
-    If downloading image fails for one of various reasons, will raise an appropriate error instead of
-    failing silently.
-
-    If file already exists, do not overwrite.
+    Raised when an image download is unsuccessful.
     """
 
     pass
+
+
+def download_image(url: str, file_path: str):
+    """
+        Download an image at specified url. This is an API agnostic function that does not
+        take an API key for a particular service. Expectation is that resource is generally
+        accesssible.
+    .
+        Attempt to save image at target file path. If file path does not exist, it will be created.
+        Default location is user's home directory. Returns the location on filesystem where image was saved.
+
+        If downloading image fails for one of various reasons, will raise an appropriate error instead of
+        failing silently.
+
+        If file already exists, do not overwrite.
+    """
+
+    destination_path = os.path.abspath(file_path)
+
+    # prevent overwriting an existing file. this is a design decision to prevent unintentional deletions
+    if os.path.exists(destination_path):
+        raise FileExistsError(f"File already exists at {destination_path}.")
+
+    # make sure there is a valid directory path to prevent file not found errors on save later
+    if not os.path.exists(os.path.basename(destination_path)):
+        os.makedirs(os.path.basename(destination_path))
+
+    # now for the good stuff. get the raw image content and use the imaging library to save to file.
+    # two main error conditions: bad http request or trying to access something that's not an image.
+    r = requests.get(url)
+    try:
+        r.raise_for_status()
+    except requests.exceptions.HTTPError:
+        raise ImageDownloadError(
+            f"Download error: something went wrong trying to access {url} (status code {r.status_code})"
+        )
+
+    try:
+        with Image.open(io.BytesIO(r.content)) as image:
+            image.save(destination_path)
+
+    except UnidentifiedImageError:
+        raise ImageDownloadError(
+            f"Download error: the target resource at {url} does not appear to be an image."
+        )
 
 
 def update_wallpaper(img_path: str, options=None) -> None:
