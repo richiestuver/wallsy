6829a54594f38e4ba619ea96b0e8e5e4216b09c7
richiestuver <stuverr@oregonstate.edu>
2021-10-03 16:45:48 -0700

    fix pytest setup teardown issue and implement tests

--- wallsy/tests/test_cli.py
+++ wallsy/tests/test_cli.py
@@ -5,19 +5,22 @@ cli.py is the entry point for the wallsy program. verify that standard
 invokation returns with correct exit code on success or failure.
 """
 
+from subprocess import run
+
 import pytest
 import click
 from click.testing import CliRunner
 
 from wallsy.cli import cli
+
 from wallsy.cli_utils.utils import import_commands
 from wallsy.cli_utils.utils import attach_commands
 
 runner = CliRunner()
 
 
-@pytest.fixture
-def subcommands(entry_point: click.Group = cli):
+@pytest.fixture(scope="module")
+def subcommands():
     """
     Import and attach all of the commands found in the /subcommands folder *without*
     invoking the entrypoint (cli).
@@ -26,12 +29,30 @@ def subcommands(entry_point: click.Group = cli):
     # setup
 
     cmds = import_commands()
-    attach_commands(cli, cmds)
 
-    yield cli
+    @click.command(name="_test")
+    def test_command(*args, **kwargs):
+        print("TEST COMMAND - I am a functioning command.")
 
-    # teardown
-    cli.commands = {}
+    cmds.append(test_command)
+
+    return cmds
+
+
+@pytest.fixture(autouse=True)
+def setup(subcommands, reset_commands, entry_point: click.Group = cli):
+    attach_commands(entry_point, subcommands)
+    yield
+    reset_commands(entry_point=entry_point)
+
+
+@pytest.fixture
+def reset_commands():
+    def inner(entry_point: click.Group = cli):
+        # teardown the commands that may have been added to clean the test environment.
+        entry_point.commands = {}
+
+    return inner
 
 
 def test_invocation_no_args():
@@ -54,22 +75,22 @@ def test_invocation_failure_no_commands(test_image):
     assert result.exit_code != 0
 
 
-def test_option_file_single_success(test_image, cli=subcommands):
+def test_option_file_single_success(test_image):
     result = runner.invoke(cli, ["--file", str(test_image.resolve()), "show"])
-
-    print("OUTPUT:", result.stdout)
 
     assert result.exit_code == 0
 
 
-def test_option_quiet(test_image, cli=subcommands):
+def test_option_quiet(test_image):
 
-    result = runner.invoke(cli, ["--file", str(test_image.resolve()), "show"])
+    result = runner.invoke(
+        cli, ["--file", str(test_image.resolve()), "--quiet", "show"]
+    )
 
-    print("OUTPUT:", result.stdout)
     assert result.exit_code == 0
 
 
-def test_no_commands():
+def test_launch_as_script_success(test_image):
 
-    assert cli.commands == {}
+    result = run(f"wallsy --file {test_image} _test".split(" "))
+    assert result.returncode == 0
